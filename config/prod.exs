97ffe0c0814d119af495f7b4cc3f72e3945b743b
Ryan Moore <ryan.moore@tagglogistics.com>
2018-10-13 13:05:33 -0500

    updates for heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :api, ApiWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
-
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 # Do not print debug messages in production
 config :logger, level: :info
 
