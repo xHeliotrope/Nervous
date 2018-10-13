452a0d0497e99bbf210387316d508c66e8a038c5
Ryan Moore <ryan.moore@tagglogistics.com>
2018-10-13 13:07:43 -0500

    fixed prod config typo, updated prod database

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,8 @@ use Mix.Config
 config :api, ApiWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,3 +63,11 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
 import_config "prod.secret.exs"
+
+
+# Configure your database
+config :api, ApiWeb.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
